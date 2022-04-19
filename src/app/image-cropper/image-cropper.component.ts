30019ffd0527df997dd3358bf69dcfdad501913b
dcartist <pbannerman13@gmail.com>
2022-04-19 11:07:33 -0400

    adding cropper

--- src/app/image-cropper/image-cropper.component.ts
+++ src/app/image-cropper/image-cropper.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Input, ElementRef } from '@angular/core';
+import Cropper from "cropperjs";
 
 @Component({
-  selector: 'app-image-cropper',
-  templateUrl: './image-cropper.component.html',
-  styleUrls: ['./image-cropper.component.scss']
+    selector: 'image-cropper',
+    templateUrl: './image-croppper.component.html',
+    styleUrls: ['./image-croppper.component.css']
 })
-export class ImageCropperComponent implements OnInit {
+export class ImageCroppperComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild("image", { static: false })
+    public imageElement: ElementRef;
 
-  ngOnInit(): void {
-  }
+    @Input("src")
+    public imageSource: string;
+
+    public imageDestination: string;
+    private cropper: Cropper;
+
+    public constructor() {
+        this.imageDestination = "";
+    }
+
+    public ngAfterViewInit() {
+        this.cropper = new Cropper(this.imageElement.nativeElement, {
+            zoomable: false,
+            scalable: false,
+            aspectRatio: 1,
+            crop: () => {
+                const canvas = this.cropper.getCroppedCanvas();
+                this.imageDestination = canvas.toDataURL("image/png");
+            }
+        });
+    }
+
+    public ngOnInit() { }
 
 }
